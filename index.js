fe8896bef358f2be6006a1afeb1db4b9910fb3b2
zhi.chen <zhi.chen@ucarinc.com>
2017-10-12 11:34:48 +0800

    优化 DOM 操作

--- index.js
+++ index.js
@@ -1,71 +1,44 @@
-// 一个简单的点赞功能
-/* const button = document.querySelector('.like-btn')
-const buttonText = button.querySelector('.like-text')
-let isLiked = false
-button.addEventListener('click', () => {
-  isLiked = !isLiked
-  if (isLiked) {
-    buttonText.innerHTML = '取消'
-  } else {
-    buttonText.innerHTML = '点赞'
-  }
-}, false) */
 
-// 结构复用
-/* class LikeButton {
-    render () {
-      return `
-        <button id='like-btn'>
-          <span class='like-text'>赞</span>
-          <span>👍</span>
-        </button>
-      `
-    }
-  }
-
-  const wrapper = document.querySelector('.wrapper')
-  const likeButton1 = new LikeButton()
-  wrapper.innerHTML = likeButton1.render()
-  
-  const likeButton2 = new LikeButton()
-  wrapper.innerHTML += likeButton2.render() */
-
-//实现简单的组件化
 const createDOMFromString = (domString) => {
     const div = document.createElement('div')
     div.innerHTML = domString
     return div
 }
 
-/* class LikeButton {
-    render () {
-      this.el = createDOMFromString(`
-        <button class='like-button'>
-          <span class='like-text'>点赞</span>
-          <span>👍</span>
-        </button>
-      `)
-      this.el.addEventListener('click', () => console.log('click'), false)
-      return this.el
-    }
-  } */
-
 class LikeButton {
     constructor () {
       this.state = { isLiked: false }
     }
 
-    changeLikeText () {
+    /* changeLikeText () {
       const likeText = this.el.querySelector('.like-text')
       this.state.isLiked = !this.state.isLiked
       likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
-    }
+    } */
+
+   /*  setState (state) {
+        this.state = state
+        this.el = this.render()
+      } */
+
+      setState (state) {
+        const oldEl = this.el
+        this.state = state
+        this.el = this.render()
+        if (this.onStateChange) this.onStateChange(oldEl, this.el)
+      } 
+  
+    changeLikeText () {
+        this.setState({
+          isLiked: !this.state.isLiked
+        })
+     }
 
     render () {
       this.el = createDOMFromString(`
         <button class='like-button'>
-          <span class='like-text'>点赞</span>
-          <span>👍</span>
+        <span class='like-text'>${this.state.isLiked ? '取消' : '点赞'}</span>
+        <span>👍</span>
         </button>
       `)
       this.el.addEventListener('click', this.changeLikeText.bind(this), false)
@@ -75,8 +48,15 @@ class LikeButton {
 
   const wrapper = document.querySelector('.wrapper')
   
-    const likeButton1 = new LikeButton()
+    /* const likeButton1 = new LikeButton()
     wrapper.appendChild(likeButton1.render())
   
     const likeButton2 = new LikeButton()
-    wrapper.appendChild(likeButton2.render())
+    wrapper.appendChild(likeButton2.render()) */
+
+    const likeButton = new LikeButton()
+    wrapper.appendChild(likeButton.render()) // 第一次插入 DOM 元素
+    likeButton.onStateChange = (oldEl, newEl) => {
+      wrapper.insertBefore(newEl, oldEl) // 插入新的元素
+      wrapper.removeChild(oldEl) // 删除旧的元素
+    }
