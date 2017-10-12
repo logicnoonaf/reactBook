680137c921af6a06a9c8e1fde8607cffd3cfd928
zhi.chen <zhi.chen@ucarinc.com>
2017-10-12 10:27:20 +0800

    实现简单的组件化

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// const button = document.querySelector('.like-btn')
-// const buttonText = button.querySelector('.like-text')
-// let isLiked = false
-// button.addEventListener('click', () => {
-//   isLiked = !isLiked
-//   if (isLiked) {
-//     buttonText.innerHTML = '取消'
-//   } else {
-//     buttonText.innerHTML = '点赞'
-//   }
-// }, false)
+// 一个简单的点赞功能
+/* const button = document.querySelector('.like-btn')
+const buttonText = button.querySelector('.like-text')
+let isLiked = false
+button.addEventListener('click', () => {
+  isLiked = !isLiked
+  if (isLiked) {
+    buttonText.innerHTML = '取消'
+  } else {
+    buttonText.innerHTML = '点赞'
+  }
+}, false) */
 
-class LikeButton {
+// 结构复用
+/* class LikeButton {
     render () {
       return `
         <button id='like-btn'>
@@ -26,4 +28,55 @@ class LikeButton {
   wrapper.innerHTML = likeButton1.render()
   
   const likeButton2 = new LikeButton()
-  wrapper.innerHTML += likeButton2.render()
+  wrapper.innerHTML += likeButton2.render() */
+
+//实现简单的组件化
+const createDOMFromString = (domString) => {
+    const div = document.createElement('div')
+    div.innerHTML = domString
+    return div
+}
+
+/* class LikeButton {
+    render () {
+      this.el = createDOMFromString(`
+        <button class='like-button'>
+          <span class='like-text'>点赞</span>
+          <span>👍</span>
+        </button>
+      `)
+      this.el.addEventListener('click', () => console.log('click'), false)
+      return this.el
+    }
+  } */
+
+class LikeButton {
+    constructor () {
+      this.state = { isLiked: false }
+    }
+
+    changeLikeText () {
+      const likeText = this.el.querySelector('.like-text')
+      this.state.isLiked = !this.state.isLiked
+      likeText.innerHTML = this.state.isLiked ? '取消' : '点赞'
+    }
+
+    render () {
+      this.el = createDOMFromString(`
+        <button class='like-button'>
+          <span class='like-text'>点赞</span>
+          <span>👍</span>
+        </button>
+      `)
+      this.el.addEventListener('click', this.changeLikeText.bind(this), false)
+      return this.el
+    }
+  }
+
+  const wrapper = document.querySelector('.wrapper')
+  
+    const likeButton1 = new LikeButton()
+    wrapper.appendChild(likeButton1.render())
+  
+    const likeButton2 = new LikeButton()
+    wrapper.appendChild(likeButton2.render())
