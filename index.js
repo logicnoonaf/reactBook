c50424931057c29119b468dd038f0b45daab5e57
zhi.chen <zhi.chen@ucarinc.com>
2017-10-12 10:04:46 +0800

    结构复用

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const button = document.querySelector('.like-btn')
-const buttonText = button.querySelector('.like-text')
-let isLiked = false
-button.addEventListener('click', () => {
-  isLiked = !isLiked
-  if (isLiked) {
-    buttonText.innerHTML = '取消'
-  } else {
-    buttonText.innerHTML = '点赞'
+// const button = document.querySelector('.like-btn')
+// const buttonText = button.querySelector('.like-text')
+// let isLiked = false
+// button.addEventListener('click', () => {
+//   isLiked = !isLiked
+//   if (isLiked) {
+//     buttonText.innerHTML = '取消'
+//   } else {
+//     buttonText.innerHTML = '点赞'
+//   }
+// }, false)
+
+class LikeButton {
+    render () {
+      return `
+        <button id='like-btn'>
+          <span class='like-text'>赞</span>
+          <span>👍</span>
+        </button>
+      `
+    }
   }
-}, false)
+
+  const wrapper = document.querySelector('.wrapper')
+  const likeButton1 = new LikeButton()
+  wrapper.innerHTML = likeButton1.render()
+  
+  const likeButton2 = new LikeButton()
+  wrapper.innerHTML += likeButton2.render()
